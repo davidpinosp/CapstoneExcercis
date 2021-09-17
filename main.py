5b0ef086a2f9b0ca09462c216eaa9c72ab486f01
davidpinosKin <71401285+davidpinosp@users.noreply.github.com>
2021-09-17 00:59:45 -0700

    sample commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # sort the array in ascending order 
 
-
+# small comment to test commit
 
 # input
-# enter = input("order the list in asc or desc ? ")
-# order
+enter = input("order the list in asc or desc ? ")
+#order
 def order(how):
     array = [1, 54, 234, 6, 6, 56, 67, 234, 234, 24, 5, 64, 2, 11, 8, 69, 6, 7678]
     if how == "asc":
@@ -13,6 +13,6 @@ def order(how):
         array.sort(reverse = True)
     return array
 
+print(order(enter))
 
-# order(enter)
 
--- testiing.py
+++ testiing.py
@@ -2,12 +2,11 @@ import unittest
 from main import order
 
 a = order("asc")
-b = order("desc")
 
 class TestOrder(unittest.TestCase):
     def test_order(self):
         self.assertEqual(a[0], 1)
-        self.assertEqual(b[0], 7678)
+
 
 
 if __name__ == '__main__':
