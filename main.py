dd8930d1122445c6ec11e6665ad4709445ae681f
davidpinosKin <71401285+davidpinosp@users.noreply.github.com>
2021-09-17 00:51:36 -0700

    adding tests

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # sort the array in ascending order 
 
 
-array = [1, 54, 234, 6, 6, 56, 67, 234, 234, 24, 5, 64, 2, 11, 8, 69, 6, 7678]
+
+# input
+# enter = input("order the list in asc or desc ? ")
+# order
+def order(how):
+    array = [1, 54, 234, 6, 6, 56, 67, 234, 234, 24, 5, 64, 2, 11, 8, 69, 6, 7678]
+    if how == "asc":
+       array.sort()
+    elif how == "desc":
+        array.sort(reverse = True)
+    return array
 
 
-array.sort()
+# order(enter)
 
-print(array)
